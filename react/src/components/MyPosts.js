8b7efe5c8758a20ebc86b73b04ecbd1d978efcb0
agnesnatasya <e0313565@u.nus.edu>
2020-05-22 23:10:38 +0800

    Draft of list of user's post

---
+++ react/src/components/MyPosts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { MyPost } from './MyPost';
+import { List, Header, Rating } from "semantic-ui-react";
+
+export const MyPosts = ({ userId, myPosts }) => {
+  return (
+    <List>
+      {myPosts
+        .map((order) => {
+          return (
+            <MyPost order={order} key={order.id} />
+          );
+        })}
+    </List>
+  );
+};
