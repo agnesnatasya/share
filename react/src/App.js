1bee3e452befc4553f7ff54d91ad07b14babaa51
agnesnatasya <e0313565@u.nus.edu>
2020-05-22 23:30:29 +0800

    Add routing to all posts

--- react/src/App.js
+++ react/src/App.js
@@ -12,13 +12,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      myOrders:[],
-      orders: [],
+      myPosts:[],
+      posts: [],
       userId: 0,
     };
     this.changeUserId = this.changeUserId.bind(this);
-    this.changeOrders = this.changeOrders.bind(this);
-    this.changeMyOrders = this.changeMyOrders.bind(this);
+    this.changePosts = this.changePosts.bind(this);
+    this.changeMyPosts = this.changeMyPosts.bind(this);
   }
 
   changeUserId(newUserId) {
@@ -27,22 +27,23 @@ class App extends Component {
     });
   }
 
-  changeOrders(newOrders) {
+  changePosts(newPosts) {
     this.setState({
-      orders: newOrders
+      posts: newPosts
     });
   }
 
-  changeMyOrders(newOrders) {
+  changeMyPosts(newPosts) {
     this.setState({
-      myOrders: newOrders
+      myPosts: newPosts
     });
   }
 
   render() {
     return (
       <Container style={{ marginTop: 40 }}>
-        <NavBar userId={this.state.userId} orders={this.state.orders} onChangeOrders={this.changeOrders} onChangeMyOrders={this.changeMyOrders} />
+        <NavBar userId={this.state.userId} posts={this.state.posts} onChangePosts={this.changePosts} onChangeMyPosts={this.changeMyPosts} />
+        <Route exact path="/posts" render={() => <Posts posts={this.state.posts} />} />
         <Route exact path="/login" render={() => <RegisterForm onChange={this.changeUserId} onChangeOrders={this.changeOrders} onChangeMyOrders={this.changeMyOrders} />} />
         <Route exact path="/sign-up" component={SignUp} />
       </Container>
