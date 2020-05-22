a21d618841b750d1bbc22e7cff32fa881818d6fa
agnesnatasya <e0313565@u.nus.edu>
2020-05-22 23:18:30 +0800

    Add states to the Post Element and to display form if neccessary

--- react/src/components/Post.js
+++ react/src/components/Post.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import { List, Header, Rating } from "semantic-ui-react";
-import { EditOrder } from "./EditOrder";
+import { EditOrder } from "./EditPost";
 import { BrowserRouter as Router, Link, Redirect } from "react-router-dom";
 
 export class Post extends Component {
@@ -8,6 +8,9 @@ export class Post extends Component {
     super(props);
         this.state = {
             showForm: false,
+            postid: this.props.post.postid,
+            title: this.props.post.title,
+            body: this.props.post.body
         };
         this.onChangeInfo = this.onChangeInfo.bind(this);
         this.onClick = this.onClick.bind(this);
@@ -30,6 +33,9 @@ export class Post extends Component {
   render() {
     return (
     <List.Item key={this.props.order.id}>
+      <Header as="h1">{this.state.title}</Header>
+      <List.Item>{this.state.body}</List.Item>
+      <List.Item>{this.state.items}</List.Item>
       <button className="ui right floated button" onClick={this.onClick}>
       Edit
       </button>
