95daa8c4af71e8e8643f14f4496fb94784884c20
agnesnatasya <e0313565@u.nus.edu>
2020-05-22 23:06:19 +0800

    Create the Link to signup and login page

--- react/src/App.js
+++ react/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SignUp } from './components/SignUp';
+import { RegisterForm } from './components/RegisterForm';
+import { NavBar } from './components/NavBar';
+import { Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myOrders:[],
+      orders: [],
+      userId: 0,
+    };
+    this.changeUserId = this.changeUserId.bind(this);
+    this.changeOrders = this.changeOrders.bind(this);
+    this.changeMyOrders = this.changeMyOrders.bind(this);
+  }
+
+  changeUserId(newUserId) {
+    this.setState({
+      userId: newUserId
+    });
+  }
+
+  changeOrders(newOrders) {
+    this.setState({
+      orders: newOrders
+    });
+  }
+
+  changeMyOrders(newOrders) {
+    this.setState({
+      myOrders: newOrders
+    });
+  }
+
+  render() {
+    return (
+      <Container style={{ marginTop: 40 }}>
+        <NavBar userId={this.state.userId} orders={this.state.orders} onChangeOrders={this.changeOrders} onChangeMyOrders={this.changeMyOrders} />
+        <Route exact path="/login" render={() => <RegisterForm onChange={this.changeUserId} onChangeOrders={this.changeOrders} onChangeMyOrders={this.changeMyOrders} />} />
+        <Route exact path="/sign-up" component={SignUp} />
+      </Container>
+    );
+  }
 }
 
 export default App;
+
+
+  
+
+ 
