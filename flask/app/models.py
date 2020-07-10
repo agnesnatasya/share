32d381520bc938dc4adb2d2f1d904a1be4be864e
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-10 19:02:54 +0800

    Create backref from user to trip

--- flask/app/models.py
+++ flask/app/models.py
@@ -6,23 +6,21 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(100))
-    trips = relationship("Trip", secondary="rides")
+    trips = relationship("Trip", secondary="rides", backref=db.backref('users'))
 
 
 class Trip(db.Model):
     __tablename__ = 'trips'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     creator = db.Column(db.String(100))
-    created_at = db.Column(db.DateTime)
-    depart_time = db.Column(db.DateTime)
+    created_at = db.Column(db.Integer)
+    depart_time = db.Column(db.Integer)
     origin = db.Column(db.String(300))
     destination = db.Column(db.String(300))
     capacity = db.Column(db.Integer)
-    users = relationship("User", secondary="rides")
 
 
-class Ride(db.Model):
-    __tablename__ = 'rides'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'))
+rides = db.Table(
+    'rides', db.Column('id', db.Integer, primary_key=True, autoincrement=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('trip_id', db.Integer, db.ForeignKey('trips.id')))
