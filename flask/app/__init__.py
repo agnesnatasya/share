4d68f1df40f46132b06ad8ae92197b9361eb9f44
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-10 19:02:32 +0800

    Enable batch migration

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -9,3 +9,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+
+with app.app_context():
+    if db.engine.url.drivername == 'sqlite':
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
