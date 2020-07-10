89666bccf1dba101f4beb75dd201a730d22a08f6
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-10 19:14:20 +0800

    Change layout of Trip element

--- react/src/components/Trip.js
+++ react/src/components/Trip.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Button, ListGroup, ProgressBar, InputGroup } from "react-bootstrap";
+import { Badge, Button, ListGroup, Card, Accordion, InputGroup } from "react-bootstrap";
+import dateFormat from 'dateformat';
 
 export class Trip extends Component {
   constructor(props) {
@@ -8,51 +9,74 @@ export class Trip extends Component {
       id: this.props.trip.id,
       creator: this.props.trip.creator,
       created_at: this.props.trip.created_at,
-      depart_time: this.props.trip.depart_time,
+      departTime: this.props.trip.depart_time,
       origin: this.props.trip.origin,
       destination: this.props.trip.destination,
       capacity: this.props.trip.capacity,
       users: this.props.trip.users,
-      buttonName: 'Join'
+      buttonName: 'Join',
+      buttonVariant: 'success'
     };
-    this.onChangeInfo = this.onChangeInfo.bind(this);
     this.onClick = this.onClick.bind(this);
   }
 
-  onClick() {
+  async onClick() {
     if (this.state.buttonName === 'Join') {
-      this.setState({ buttonName: 'Cancel' })
+      await fetch("/join-trip/" + localStorage.getItem('email') + '/' + this.state.id, {
+        method: "POST",
+      });
+      this.setState({ buttonName: 'Cancel', buttonVariant: 'secondary' })
     } else {
-      this.setState({ buttonName: 'Join' })
+      await fetch("/quit-trip/" + localStorage.getItem('email') + '/' + this.state.id, {
+        method: "POST",
+      });
+      this.setState({ buttonName: 'Join', buttonVariant: 'success' })
     }
 
   }
 
-  onChangeInfo(newInfo) {
-    this.setState({ body: newInfo.body })
-  }
 
   render() {
     return (
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Card.Header>
+            <InputGroup>
+              <InputGroup.Prepend className="input-group-left">
+                <InputGroup.Text id="basic-addon1"><div class="text-wrap">{this.state.origin}</div></InputGroup.Text>
+              </InputGroup.Prepend>
+              <InputGroup.Prepend className="input-group-middle">
+                <InputGroup.Text id="basic-addon1"><div class="text-wrap">{this.state.destination}</div></InputGroup.Text>
+              </InputGroup.Prepend>
+              <InputGroup.Prepend className="input-group-right">
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  <div class="text-wrap">Details..</div>
+                </Accordion.Toggle>
+              </InputGroup.Prepend>
+            </InputGroup>
+          </Card.Header>
 
-      <ListGroup key={this.props.trip.id} variant="flush">
-        <ListGroup.Item>
-          <InputGroup>
-            <InputGroup.Prepend className="input-group-left">
-              <InputGroup.Text id="basic-addon1">{this.state.origin}</InputGroup.Text>
-            </InputGroup.Prepend>
-            <InputGroup.Prepend className="input-group-right">
-              <InputGroup.Text id="basic-addon1">{this.state.destination}</InputGroup.Text>
-            </InputGroup.Prepend>
-          </InputGroup>
-        </ListGroup.Item>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <ListGroup horizontal>
+                <ListGroup.Item className="info-holder">
+                  <Button disabled={true} variant="info" size="sm">
+                    Depart time: {dateFormat(this.state.departTime, "mmmm dS, yyyy HH:MM")}
+                  </Button> {' '}
+                  <Button disabled={true} variant="info" size="sm">Driver: {this.state.creator}</Button>
+                </ListGroup.Item>
 
-        <ListGroup.Item className="button-holder">
-          <Button variant="success">{this.state.buttonName}</Button>{' '}
-        </ListGroup.Item>
-      </ ListGroup >
-
-
+                <ListGroup.Item className="button-holder">
+                  <Button
+                    variant={this.state.buttonVariant}
+                    onClick={this.onClick}
+                  >{this.state.buttonName}</Button>{' '}
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion >
     );
   }
 }
