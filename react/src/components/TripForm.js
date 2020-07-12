33769dd1359975aea30be2c2f72c1a9acf32e752
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-13 02:41:22 +0800

    Fix logic to display validation form messages

--- react/src/components/TripForm.js
+++ react/src/components/TripForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Form, Button, Container, Row, Col, Toast, Card } from "react-bootstrap";
 import DateTimePicker from 'react-datetime-picker';
 
 export const TripForm = ({ userId }) => {
@@ -7,86 +7,198 @@ export const TripForm = ({ userId }) => {
   const [destination, setDestination] = useState("");
   const [capacity, setCapacity] = useState("");
   const [departTimeFormat, setDepartTimeFormat] = useState(new Date())
+  const [errors, setErrors] = useState({
+    'origin': 'Origin cannot be empty!',
+    'destination': 'Destination cannot be empty!',
+    'capacity': 'Capacity cannot be empty!',
+  });
+  const [showSuccessMessage, setShowSuccessMessage] = useState("")
+
+  const [touched, setTouched] = useState({
+    'origin': false,
+    'destination': false,
+    'capacity': false
+  });
+
+  const successMessage = () => (
+    <Toast>
+      <Toast.Header>
+        <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+        <strong className="mr-auto">Bootstrap</strong>
+        <small>11 mins ago</small>
+      </Toast.Header>
+      <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+    </Toast>
+  );
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    switch (name) {
+      case 'origin':
+        setOrigin(value)
+        errors.origin =
+          value.length <= 0
+            ? 'Origin cannot be empty!'
+            : '';
+        break;
+      case 'destination':
+        setDestination(value)
+        errors.destination =
+          value.length <= 0
+            ? 'Destination cannot be empty!'
+            : '';
+        break;
+      case 'capacity':
+        setCapacity(value)
+        console.log(parseInt(value))
+        errors.capacity =
+          value.length <= 0
+            ? 'Capacity cannot be empty!'
+            :
+            parseInt(value) != parseInt(value)
+              ? 'Capacity should be a number'
+              : '';
+        break;
+      default:
+        break;
+    }
+  }
+
+  const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+      // if we have an error string set valid to false
+      (val) => val.length > 0 && (valid = false)
+    );
+    return valid;
+  }
+
+  const shouldMarkError = (field) => {
+    const hasError = errors[field];
+    const shouldShow = touched[field];
+    return hasError ? shouldShow : false;
+  };
+
+  const handleBlur = (field) => (evt) => {
+    setTouched({ ...touched, [field]: true })
+  }
 
   return (
-    <Container>
-      <h1>Create a new Trip!</h1>
-      <Form.Group as={Row}>
-        <Form.Label column sm="2">
-          Origin
+    <div>
+      {showSuccessMessage ? (
+        <Toast>
+          <Toast.Body>Trip created successfully!</Toast.Body>
+        </Toast>
+      )
+        :
+        <Toast className="hiddenToast">
+          <Toast.Body>Hidden Message</Toast.Body>
+        </Toast>}
+      <Container>
+        <Card className="form-card">
+          <Card.Body>
+            <h1>Create a new Trip!</h1>
+            <Form.Group as={Row}>
+              <Form.Label column lg="2">
+                Origin
         </Form.Label>
-        <Col sm="10">
-          <Form.Control
-            placeholder="From here.."
-            value={origin}
-            onChange={(e) => setOrigin(e.target.value)}
-          />
-        </Col>
-      </Form.Group>
+              <Col lg="10">
+                <Form.Control
+                  className={shouldMarkError('origin') ? "error" : ""}
+                  onBlur={handleBlur('origin')}
+                  placeholder="From here.."
+                  value={origin}
+                  name='origin'
+                  onChange={handleChange}
+                />
+                <p className="errorMessage">{shouldMarkError('origin') ? errors.origin : ""}</p>
+              </Col>
+            </Form.Group>
 
-      <Form.Group as={Row}>
-        <Form.Label column sm="2">
-          Destination
+            <Form.Group as={Row}>
+              <Form.Label column lg="2">
+                Destination
         </Form.Label>
-        <Col sm="10">
-          <Form.Control
-            placeholder="To here.."
-            value={destination}
-            onChange={(e) => setDestination(e.target.value)}
-          />
-        </Col>
-      </Form.Group>
+              <Col lg="10">
+                <Form.Control
+                  className={shouldMarkError('destination') ? "error" : ""}
+                  onBlur={handleBlur('destination')}
+                  placeholder="To here.."
+                  value={destination}
+                  name='destination'
+                  onChange={handleChange}
+                />
+                <p className="errorMessage">{shouldMarkError('destination') ? errors.destination : ""}</p>
+              </Col>
+            </Form.Group>
 
-      <Form.Group as={Row}>
-        <Form.Label column sm="2">
-          Capacity
+            <Form.Group as={Row}>
+              <Form.Label column lg="2">
+                Capacity
         </Form.Label>
-        <Col sm="10">
-          <Form.Control
-            placeholder="Number of seats left"
-            value={capacity}
-            onChange={(e) => setCapacity(e.target.value)}
-          />
-        </Col>
-      </Form.Group>
+              <Col lg="10">
+                <Form.Control
+                  className={shouldMarkError('capacity') ? "error" : ""}
+                  onBlur={handleBlur('capacity')}
+                  placeholder="Number of seats left"
+                  value={capacity}
+                  name='capacity'
+                  onChange={handleChange}
+                />
+                <p className="errorMessage">{shouldMarkError('capacity') ? errors.capacity : ""}</p>
+              </Col>
+            </Form.Group>
 
-      <Form.Group as={Row}>
-        <Form.Label column sm="2">
-          Capacity
+            <Form.Group as={Row}>
+              <Form.Label column lg="2">
+                Departure Time
         </Form.Label>
-        <Col sm="10">
-          <DateTimePicker
-            variant="round"
-            value={departTimeFormat}
-            onChange={setDepartTimeFormat}
-          />
-        </Col>
-      </Form.Group>
-      <Button
-        onClick={async () => {
-          const createdAt = new Date().getTime();
-          const departTime = departTimeFormat.getTime();
-          const trip = { createdAt, departTime, origin, destination, capacity };
-          const response = await fetch("/new-trip/" + localStorage.getItem('email'), {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(trip),
-          });
+              <Col lg="10">
+                <DateTimePicker
+                  variant="round"
+                  value={departTimeFormat}
+                  onChange={setDepartTimeFormat}
+                />
+              </Col>
+            </Form.Group>
+            <Button
+              variant="success"
+              onClick={async () => {
+                if (!validateForm(errors)) {
+                  console.error('Invalid Form')
+                } else {
+                  console.info('Valid Form')
 
-          if (response.ok) {
-            console.log("response worked");
-            setOrigin("");
-            setDestination("");
-            setCapacity("");
-            setDepartTimeFormat(new Date())
-          }
-        }}
-      >
-        Submit
+                  const createdAt = new Date().getTime();
+                  const departTime = departTimeFormat.getTime();
+                  const trip = { createdAt, departTime, origin, destination, capacity };
+                  const response = await fetch("/new-trip/" + localStorage.getItem('email'), {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(trip),
+                  });
+
+                  if (response.ok) {
+                    setOrigin("");
+                    setDestination("");
+                    setCapacity("");
+                    setDepartTimeFormat(new Date())
+                    setShowSuccessMessage(true)
+                  }
+                }
+              }
+              }
+            >
+              Submit
           </Button>
-    </Container >
+          </Card.Body>
+        </Card>
+      </Container >
+    </div>
+
   );
 };
 
